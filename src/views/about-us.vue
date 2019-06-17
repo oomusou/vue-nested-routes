e305cc9a2290d1011a027010987805b4ccbd815d
oomusou <oomusou@gmail.com>
2019-06-17 17:06:48 +0800

    Initial commit

---
+++ src/views/about-us.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Us</h2>
+
+</template>
+
+<script>
+export default {
+  name: "about-us"
+}
+</script>
---
+++ src/views/about-you.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>You</h2>
+
+</template>
+
+<script>
+export default {
+  name: "about-you"
+}
+</script>
